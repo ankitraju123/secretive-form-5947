ccf486141564309267923c37e01bfecd1002d541
Muhammed sadique <mhdsadique8@gmail.com>
2023-01-19 11:35:23 +0530

    day2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "images-static.nykaa.com",
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
+// {
+//   protocol: 'https',
+//   hostname:'images-static.nykaa.com',
+//   port: '',
+//   pathname: '**',
+// }
