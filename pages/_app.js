fe4b050f73897558462d45f4b41f52c882752855
geraltyen <girireddyanna@gmail.com>
2023-01-20 12:35:02 +0530

    added cartpage

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import '@/styles/globals.css'
+
 import { ChakraProvider } from '@chakra-ui/react'
 import "../styles/homepage.css"
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
+
 export default function App({ Component, pageProps }) {
+ 
   return <>
 
   <ChakraProvider>
