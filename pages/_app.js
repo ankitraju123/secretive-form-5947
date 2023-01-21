fef0b2bc146419e8b78eb368f65b61aa157fa527
geraltyen <girireddyanna@gmail.com>
2023-01-21 15:05:52 +0530

    added cart css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 
 import { ChakraProvider } from '@chakra-ui/react'
 import "../styles/homepage.css"
+import "../styles/cartproduct.css"  
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
 
