98783281e3969bc6b7314fb0d2ca0c7fecae0abb
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-21 14:53:08 +0530

    last day

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,20 @@ import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import {GoogleOAuthProvider} from "@react-oauth/google"
+import "../styles/homepage.css"
+import {Provider} from 'react-redux'
+import { store } from '@/store/store'
+
 export default function App({ Component, pageProps }) {
     return <>
         <GoogleOAuthProvider clientId="122806122719-hsoe2eocu4ilnidqk645ui83d5fiaus8.apps.googleusercontent.com">
             <ChakraProvider>
-                <Navbar/>
-                <Component {...pageProps} />
+                <Navbar />
+                
+  <Provider store={store}>
+
+                    <Component {...pageProps} />
+                    </Provider>
                 <Footer/>
         </ChakraProvider>
         </GoogleOAuthProvider>
